e6b3afe0e1c0e2654f498a2c8bb45e34275bebd2
vmik7 <vmik7@yandex.ru>
2021-03-30 12:39:40 +0300

    Вёрстка

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,13 +10,21 @@
     <link rel="stylesheet" href="styles/reset.css">
     <link rel="stylesheet" href="styles/style.css">
 </head>
-<body class="page">
-    <div class="page__container">
-        <h1 class="page__header">
-            Расчет медиан произвольного треугольника
-        </h1>
 
-        <div class="page__content content">
+<body class="page">
+    <header class="page__header">
+        <div class="page__container">
+            <h1 class="page__title">
+                Расчет медиан произвольного треугольника
+            </h1>
+            <p class="page__subtitle">
+                Лабораторная работа №3, 3 вариант
+            </p>
+        </div>
+    </header>
+
+    <main class="page__content content">
+        <div class="page__container content__wrapper">
             <div class="content__left">
                 <h2 class="content__header">
                     Входные данные
@@ -25,23 +34,24 @@
                         <legend class="form__legend">
                             Точка A
                         </legend>
-                        <input type="number" name="ax" placeholder="Координата X точки A" required>
-                        <input type="number" name="ay" placeholder="Координата Y точки A" required>
+                        <input class="form__input" type="number" name="ax" placeholder="Координата X точки A" required>
+                        <input class="form__input" type="number" name="ay" placeholder="Координата Y точки A" required>
                     </fieldset>
                     <fieldset class="form__fieldset">
                         <legend class="form__legend">
                             Точка B
                         </legend>
-                        <input type="number" name="bx" placeholder="Координата X точки B" required>
-                        <input type="number" name="by" placeholder="Координата Y точки B" required>
+                        <input class="form__input" type="number" name="bx" placeholder="Координата X точки B" required>
+                        <input class="form__input" type="number" name="by" placeholder="Координата Y точки B" required>
                     </fieldset>
                     <fieldset class="form__fieldset">
                         <legend class="form__legend">
                             Точка C
                         </legend>
-                        <input type="number" name="cx" placeholder="Координата X точки C" required>
-                        <input type="number" name="cy" placeholder="Координата Y точки C" required>
+                        <input class="form__input" type="number" name="cx" placeholder="Координата X точки C" required>
+                        <input class="form__input" type="number" name="cy" placeholder="Координата Y точки C" required>
                     </fieldset>
+                    <button class="form__submit" type="submit">Расчитать</button>
                 </form>
             </div>
             <div class="content__right">
@@ -59,7 +69,13 @@
                 </div>
             </div>
         </div>
-        
-    </div>
+    </main>
+
+    <footer class="page__footer">
+        <div class="page__container">
+            Designed by vmik7 [vmik7@yandex.ru]
+        </div>
+    </footer>
 </body>
+
 </html>
